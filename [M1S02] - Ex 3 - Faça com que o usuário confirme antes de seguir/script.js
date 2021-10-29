97731d0393d66c0932723c0a4641cff65767e380
guilherme-cittadela <93407595+guilherme-cittadela@users.noreply.github.com>
2021-10-29 20:39:46 -0300

    Delete [M1S02] - Ex 3 - Faça com que o usuário confirme antes de seguir directory

--- [M1S02] - Ex 3 - Faça com que o usuário confirme antes de seguir/script.js
+++
@@ -1,7 +0,0 @@
-
-function avancarCLique(){
-
-    if (window.confirm("Você realmente quer sair?")) {
-        window.open("https://developer.mozilla.org/pt-BR/docs/Web/API/window/confirm", "Obrigado pela visita!");
-    }
-}
