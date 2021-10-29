f0970ab0cf9105d85fdbcd92c0c1efcf59507496
guicittadela <guicittadela@hotmail.com>
2021-10-29 20:06:23 -0300

    exercicios segunda semana - 03

---
+++ [M1S02] - Ex 3 - Faça com que o usuário confirme antes de seguir/script.js
@@ -0,0 +1,7 @@
+
+function avancarCLique(){
+
+    if (window.confirm("Você realmente quer sair?")) {
+        window.open("https://developer.mozilla.org/pt-BR/docs/Web/API/window/confirm", "Obrigado pela visita!");
+    }
+}
