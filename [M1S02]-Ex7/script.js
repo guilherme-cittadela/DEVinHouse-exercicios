b22449239317a71733e9c6d412c7e770120f25b4
guicittadela <guicittadela@hotmail.com>
2021-11-02 11:25:52 -0300

    exercicios segunda semana- 07

--- [M1S02]-Ex7/script.js
+++ [M1S02]-Ex7/script.js
@@ -1,4 +1,5 @@
 let ultimoNome = prompt("Qual seu último nome?")
+upperName = ultimoNome.toUpperCase()
 let tamanho = ultimoNome.length
 
-window.alert("Olá " + ultimoNome.toUpperCase )
+window.alert(`Olá ${upperName}. Seu sobrenome contém ${tamanho} letras!`)
