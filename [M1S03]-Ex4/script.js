c00dc9543f0d10a6aef1ec5da12e94ad4ee89c34
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2021-12-30 13:58:23 -0300

    exercicios terceira semana - 05

--- [M1S03]-Ex4/script.js
+++ [M1S03]-Ex4/script.js
@@ -2,8 +2,18 @@ var data = new Date()
 let hour = data.getHours()
 let minutes = data.getMinutes()
 let seconds = data.getSeconds()
-
 let time = document.getElementById("acess-time")
+
 window.onload = function(){
     time.innerText=`Acesso realizado as ${hour}:${minutes}`
+    
 }
+
+function displayTime(){
+    var data = new Date()
+    let actualTime = data.toLocaleTimeString()
+    document.getElementById("actual-time").textContent = `Hora atual: ${actualTime}`
+}
+
+let createClock = setInterval(displayTime, 1000)
+
