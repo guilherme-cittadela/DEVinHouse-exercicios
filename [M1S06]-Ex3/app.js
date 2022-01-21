0b7c4430a7b5850a239c47876422985a7743d178
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2022-01-21 19:08:06 -0300

    exercicios semana 06 - 04

--- [M1S06]-Ex3/app.js
+++ [M1S06]-Ex3/app.js
@@ -5,12 +5,17 @@ const arrayInvert = (arr, inverted) =>{
     for(let v = arr.length - 1 ; v >=0; --v)
     inverted.push(arr[v])
     document.querySelector('#result').innerHTML=`
-    Array normal:    ${arr}  <br> 
-    Array invertido: ${inverted}
+    <p>Array normal:    ${arr}  <br> 
+    Array invertido: ${inverted}</p>
     `
 }
 
 window.onload = () => {
-    console.log(arrayInvert(array, invertedArray))
+    arrayInvert(array, invertedArray)
+    
 
 }
+
+const found = array.find (value => value > 5)
+document.querySelector('#result2').innerHTML = `
+    <p>O primeiro número maior que 5 no array é ${found} `
