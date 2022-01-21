54b5e898f911689452ff16b6c7183ebbd4162ee8
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2022-01-21 18:03:15 -0300

    exercicios semana 06 - 02

--- [M1S06]-Ex1/app.js
+++ [M1S06]-Ex1/app.js
@@ -1,8 +1,30 @@
 const btn = document.querySelector('#botao-ola')
-
+const btn2 = document.querySelector('#botao-ola2')
+let nome = document.querySelector('#nome')
 
 btn.onclick = () =>{
-    document.querySelector('#result').innerHTML = `
-        <p>Olá, mundo!</p>
-    `
+    helloMessage()
 }
+
+btn2.onclick = () =>{
+    helloMessage2(nome.value)
+    nome.value = ""
+}
+
+const helloMessage = () =>{
+    document.querySelector('#result').innerHTML = 
+             `<p>Olá, mundo!</p>`
+}
+
+const helloMessage2 = value => {
+    if(value != ""){
+        document.querySelector('#result2').innerHTML = 
+             `<p>Olá, ${value}!</p>`
+    }
+    else{
+        alert("Digite seu nome!")
+    }
+    
+}
+
+
