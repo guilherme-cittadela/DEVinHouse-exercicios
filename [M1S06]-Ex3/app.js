2c5b2436b086af0097a031f9489a392842010e75
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2022-01-22 10:07:14 -0300

    exercicios semana 06 - 05

--- [M1S06]-Ex3/app.js
+++ [M1S06]-Ex3/app.js
@@ -1,5 +1,6 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 let invertedArray = []
+let oddArray = []
 
 const arrayInvert = (arr, inverted) =>{
     for(let v = arr.length - 1 ; v >=0; --v)
@@ -12,6 +13,7 @@ const arrayInvert = (arr, inverted) =>{
 
 window.onload = () => {
     arrayInvert(array, invertedArray)
+    oddNumbers(array, oddArray)
     
 
 }
@@ -19,3 +21,14 @@ window.onload = () => {
 const found = array.find (value => value > 5)
 document.querySelector('#result2').innerHTML = `
     <p>O primeiro número maior que 5 no array é ${found} `
+
+const oddNumbers = (arr,odd) => {
+    arr.filter((number) =>{
+        if(number % 2 !==0){
+            odd.push(number)
+        }
+    })
+    document.querySelector('#result3').innerHTML = `
+        <p>os números ímpares do array são: [${odd}]
+    `
+}
