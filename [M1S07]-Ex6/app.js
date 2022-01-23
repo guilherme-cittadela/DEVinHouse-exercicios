ffec0114af81254c7cabc65f5dc176b7617fc737
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2022-01-22 21:05:39 -0300

    exercicios semana 07 - 6.1

--- [M1S07]-Ex6/app.js
+++ [M1S07]-Ex6/app.js
@@ -2,13 +2,16 @@ let result = document.querySelector('#result')
 const btn = document.querySelector('#busca-btn')
 
 async function buscaCep() {
-    let cep = document.querySelector('#cep')
-    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json`)
-    const obj = await response.json()
-    console.log(obj)
-    result.innerHTML = `
-    Endereço: ${obj.logradouro}, ${obj.bairro}, ${obj.localidade} - ${obj.uf}
-    `
-    cep.value = ''
+    try {
+        let cep = document.querySelector('#cep')
+        const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json`)
+        const obj = await response.json()
+        console.log(obj)
+        result.innerHTML = `
+        Endereço: ${obj.logradouro}, ${obj.bairro}, ${obj.localidade} - ${obj.uf}
+        `
+        cep.value = ''
+    } catch (error) {
+        console.error('Pegou: ' + error)    }
 }
 btn.addEventListener('click', buscaCep)
